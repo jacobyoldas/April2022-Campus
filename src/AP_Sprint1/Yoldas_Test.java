a3dcf69339e7b97ab90a7183b6cc406f82a4bc4b
jacobyoldas <yasinyoldas6666@gmail.com>
2022-08-01 22:48:03 -0400

    Last Update of TC_CourseIcon_1

--- src/AP_Sprint1/Yoldas_Test.java
+++ src/AP_Sprint1/Yoldas_Test.java
@@ -5,6 +5,6 @@ public class Yoldas_Test {
   public static void main(String[] args) {
     System.out.println("Ssssssss");
     System.out.println("Tsssssss");
-    System.out.println("Hhhhhhhhhh");
+
   }
 }
