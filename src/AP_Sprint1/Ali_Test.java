22e3580d2aa133c618fa2010c7461603d6ec13e5
mir_m <xcess0213@gmail.com>
2022-08-02 22:40:56 -0400

    Ali testing

--- src/AP_Sprint1/Ali_Test.java
+++
@@ -1,9 +0,0 @@
-package AP_Sprint1;
-
-public class Ali_Test {
-    public static void main(String[] args) {
-
-
-        System.out.println("Testing for Soft skills");
-    }
-}
---
+++ src/AP_Sprint1/Ali_Test_SoftSkills.java
@@ -0,0 +1,10 @@
+package AP_Sprint1;
+
+public class Ali_Test_SoftSkills {
+
+    public static void main(String[] args) {
+
+
+            System.out.println("Testing for Soft skills");
+    }
+}
