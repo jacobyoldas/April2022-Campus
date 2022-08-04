4d33487c1b8e3739a9855c4c24dbc1bec9070e92
talalshatra@yahoo.com <talalshatra1966>
2022-08-04 18:24:23 -0400

    Talal_TC_New

--- src/AP_Sprint1/Talal_testCase.java
+++ src/AP_Sprint1/Talal_testCase.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.interactions.Actions;
 import java.util.concurrent.TimeUnit;
 
 public class Talal_testCase {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
         System.setProperty("webdriver.chrome.driver", "C:\\Talal\\chromedriver.exe");
@@ -34,9 +34,11 @@ public class Talal_testCase {
         WebElement userIcon = driver.findElement(By.cssSelector("button[class='mat-focus-indicator mat-menu-trigger user-button mat-button mat-button-base ng-star-inserted']"));
         userIcon.click();
 
-        WebElement languageIcon = driver.findElement(By.cssSelector("button[class='mat-focus-indicator mat-menu-trigger language-button mat-menu-item mat-menu-item-submenu-trigger ng-tns-c186-5 ng-star-inserted']"));
+
+        WebElement languageIcon = driver.findElement(By.xpath("//span[text()='Language']"));
         Actions actions = new Actions(driver);
         actions.moveToElement(languageIcon).perform();
+
 
         driver.quit();
     }
