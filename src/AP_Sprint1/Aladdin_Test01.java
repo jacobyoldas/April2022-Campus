98be892a72939bf7dbe88f89a366d85b2c71f311
jacobyoldas <yasinyoldas6666@gmail.com>
2022-08-04 13:15:59 -0400

    Last Update of TC_CourseIcon_1

--- src/AP_Sprint1/Aladdin_Test01.java
+++ src/AP_Sprint1/Aladdin_Test01.java
@@ -3,7 +3,7 @@ package AP_Sprint1;
 public class Aladdin_Test01 {
     public static void main(String[] args) {
 
-        System.out.println("hello world");// hii
+        System.out.println("hello world");// wharse
 
 
 
